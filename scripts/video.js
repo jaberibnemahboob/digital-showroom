71fe9da5d08d626ac282268f19e31118d56dd19d
Jaber Ibne Mahboob <jaber.ibne.mahboob@gmail.com>
2017-10-19 18:28:39 +0200

    width 3

--- scripts/video.js
+++ scripts/video.js
@@ -1,8 +1,17 @@
 let homePageIntroVideo = document.querySelector('.introVideoSection video');
-let homePageIntroVideo_duration = homePageIntroVideo.duration;
+let homePageMainContent = document.querySelector("main");
+let homePageHeaderContent = document.querySelector(".topHeaderSection");
+
+homePageMainContent.style.display = "none";
+homePageHeaderContent.style.display = "none";
+
 homePageIntroVideo.addEventListener('ended',function(e){
     document.querySelector(".introVideoSection").style.display = "none";
+    homePageMainContent.style.display = "block";
+    homePageHeaderContent.style.display = "block";
 },false);
+
+
 setTimeout(fadeOutHomePageIntroVideo,300);
 function fadeOutHomePageIntroVideo(){
     if(homePageIntroVideo.currentTime > (homePageIntroVideo.duration - 1.2)){
