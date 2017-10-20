bcd57c922d15a0f8283e2c73668e6cdfe06fa7bf
Jaber Ibne Mahboob <jaber.ibne.mahboob@gmail.com>
2017-10-20 10:07:13 +0200

    skip btn

--- docs/scripts/video.js
+++ docs/scripts/video.js
@@ -1,11 +1,24 @@
 let homePageIntroVideo = document.querySelector('.introVideoSection video');
 let homePageMainContent = document.querySelector("main");
 let homePageHeaderContent = document.querySelector(".topHeaderSection");
+let skipBtnContent = document.querySelector(".skip-btn").parentElement;
 
 homePageMainContent.style.display = "none";
 homePageHeaderContent.style.display = "none";
 
+
+setTimeout(function(){homePageIntroVideo.play();},500);
+homePageIntroVideo.addEventListener('click',function(e){
+    homePageIntroVideo.play();
+});
+
 homePageIntroVideo.addEventListener('ended',function(e){
+    document.querySelector(".introVideoSection").style.display = "none";
+    homePageMainContent.style.display = "block";
+    homePageHeaderContent.style.display = "block";
+},false);
+
+skipBtnContent.addEventListener('click',function(e){
     document.querySelector(".introVideoSection").style.display = "none";
     homePageMainContent.style.display = "block";
     homePageHeaderContent.style.display = "block";
